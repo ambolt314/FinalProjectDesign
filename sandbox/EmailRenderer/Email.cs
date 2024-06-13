4c1e0df963fe8352051305e6f89f7911427cbc06
Andrew Bolt <40577677+ambolt314@users.noreply.github.com>
2024-06-13 18:12:48 +0100

    Added web project

---
+++ sandbox/EmailRenderer/Email.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmailRenderer
+{
+    public class Email
+    {
+        public string Type { get; set; }
+        public string From { get; set; }
+        public string FromName { get; set; }
+        public string To { get; set; }
+        public string ToName { get; set; }
+        public string CC { get; set; }
+        public string Subject { get; set; }
+        public string Body { get; set; }
+    }
+}
---
+++ sandbox/EmailRenderer/MainWindow.xaml.cs
@@ -0,0 +1,112 @@
+﻿using System.IO;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EmailRenderer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Btn_GenerateInvitationEmail_Click(object sender, RoutedEventArgs e)
+        {
+            renderEmail($"/visits-service/email/invitation-email?visitorID={Txt_InvitationVisitorID.Text}");
+        }
+
+        private void Btn_GenerateCLFVisitConfirmationEmail_Click(object sender, RoutedEventArgs e)
+        {
+            renderEmail($"/visits-service/email/cl-visit-confirmation?visitorID={Txt_CLFVisitorID.Text}&purposeID={Txt_CLFPurposeID.Text}&sessionID={Txt_CLFSessionID.Text}");
+        }
+
+        private void Btn_GenerateInsuranceNotification_Click(object sender, RoutedEventArgs e)
+        {
+            renderEmail($"/visits-service/email/insurance-notificationl?visitID={Txt_InsuranceVisitID.Text}");
+        }
+
+        
+
+        private void Btn_SendEmail_Click(object sender, RoutedEventArgs e)
+        {
+            if (Txt_From.Text == "" || Txt_To.Text == "" || Txt_Cc.Text == "" || Txt_Subject.Text == "" || Txt_Body.Text == "")
+            {
+                Lbl_Warning.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                MessageBox.Show($"Email successfully sent at {DateTime.Now}");
+            }
+
+        }
+
+
+        private async void renderEmail(string uri)
+        {
+
+            loadProgress();
+            string AUTH_TOKEN = "6576b737-41e5-41af-a0ad-3d8e94ad99d2";
+
+            HttpClient client = new HttpClient();
+
+            client.BaseAddress = new Uri("http://localhost:4012");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json"));
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", AUTH_TOKEN);
+
+            Email email = null;
+            HttpResponseMessage response = await client.GetAsync(uri);
+            
+            if (response.IsSuccessStatusCode)
+            {
+                email = await response.Content.ReadAsAsync<Email>();
+
+                Lbl_Warning.Visibility = Visibility.Hidden;
+                Txt_From.Text = $"{email.FromName} <{email.From}>";
+                Txt_To.Text = $"{email.ToName} <{email.To}>";
+                Txt_Cc.Text = email.CC;
+                Txt_Subject.Text = email.Subject;
+                Txt_Body.Text = email.Body;
+            }
+            else
+            {
+                pbStatus.Value = 0;
+                Lbl_Warning.Content = "Email could not be generated";
+                Lbl_Warning.Visibility = Visibility.Visible;
+                Txt_From.Text = "";
+                Txt_To.Text = "";
+                Txt_Cc.Text = "";
+                Txt_Subject.Text = "";
+                Txt_Body.Text = "";
+            }
+
+        }
+
+        private void loadProgress()
+        {
+            pbStatus.Value = 0;
+            while (pbStatus.Value < 100)
+            {
+                Thread.Sleep(10);
+                pbStatus.Value += 10;
+            }
+        }
+
+
+    }
+}
