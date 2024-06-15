79212b2e668da4bc2b77ad20f2acb637655d7916
Andrew Bolt <40577677+ambolt314@users.noreply.github.com>
2024-06-15 16:23:56 +0100

    WIP

--- sandbox/EmailRenderer/MainWindow.xaml.cs
+++ sandbox/EmailRenderer/MainWindow.xaml.cs
@@ -1,16 +1,6 @@
-﻿using System.IO;
-using System.Net.Http;
+﻿using System.Net.Http;
 using System.Net.Http.Headers;
-using System.Text;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace EmailRenderer
 {
